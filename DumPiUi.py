03c5bb697bea60aaee9c0725cf342854752049c8
Bobrov-I <118316125+Bobrov-I@users.noreply.github.com>
2023-11-25 05:47:55 +0300

    Update DumPiUi.py

--- DumPiUi.py
+++ DumPiUi.py
@@ -23,9 +23,9 @@ def load_models():
     model3 = YOLO("models/det_garbage_aug.pt")
     model4 = YOLO("models/cls_correct.pt")
     model5 = YOLO("models/cls_det_garbage_n_aug.pt")
-    model_ans_det1 = YOLO("models/model_ans_det1.pt") #n
-    model_ans_det2 = YOLO("models/model_ans_det2.pt") #n
-    model_ans_det3 = YOLO("models/model_ans_det3.pt") #m
+    model_ans_det1 = YOLO("models/cls_ans_det1.pt") #n
+    model_ans_det2 = YOLO("models/cls_ans_det2.pt") #n
+    model_ans_det3 = YOLO("models/cls_ans_det3.pt") #m
     model_ans_seg1 = YOLO("models/cls_ans_seg2.pt") #n
     model_ans_seg2 = YOLO("models/cls_ans_seg2.pt") #n
     model_ans_seg3 = YOLO("models/cls_ans_seg2.pt") #m
@@ -41,12 +41,26 @@ def main():
     #Функция загрузки файла
     def load():
         load_type = ['jpg','png','jpeg','mkv','mp4','mpg','mpeg','mpeg4','zip']
-        uploaded_data = st.file_uploader(label="Выберите файл для распознавания",type=load_type)
+        route = "accurate" #выбор типа цикла 'fast' или 'accurate'.
+        ensemle = False #используется ли ансамбль.
+        col1, col2 = st.columns(2)
+        with col1:
+            on = st.toggle('Ускорить? с потерей качества')
+            if on:
+                route = "fast"
+        with col2:
+            on = st.toggle('Подключть ансамбль моделей?')
+            if on:
+                ensemle = True 
+
+        uploaded_data = st.file_uploader(label="Выберите файл для распознавания",type=load_type)       
+
         if uploaded_data is not None:
             file_type = get_file_type(uploaded_data.name)
             if any(file_type == i for i in load_type[:3]):
                 image_original = load_img(uploaded_data)
-                image_cycle, res_cycle = run_cycle(models, image_original, type = "image")
+                type = "image"
+                image_cycle, res_cycle = run_cycle(models, image_original, type, ensemle, route)
                 #Вывод после картинки,  нолик - просто потому что---------------------------------
                 #Вывод номеров классов - тупа словарь ------------------------------------------------
                 st.text(res_cycle[0].names)
@@ -60,7 +74,7 @@ def main():
                 type(image_cycle)
                 st.image(image_cycle)
 
-            elif file_type == 'zip':
+            elif file_type == load_type[8]:
                 if uploaded_data is not None:
                     temp_dir = tempfile.mkdtemp()
 
@@ -70,7 +84,8 @@ def main():
                     #Всё как у видео, только выводит столько раз подряд, сколько было видео
                     for filename in os.listdir(temp_dir):
                         file_path = os.path.join(temp_dir, filename)
-                        results_list = run_cycle(models, file_path, type = "video")
+                        type = "video"
+                        results_list = run_cycle(models, file_path, type, ensemle, route)
                         #Вывод после видоса, первый нолик - номер в списке, второй нолик - просто потому что--
                         #Вывод номеров классов - тупа словарь ------------------------------------------------
                         st.text(results_list[0][0].names)
@@ -81,8 +96,9 @@ def main():
 
                     shutil.rmtree(temp_dir)
 
-            elif any(file_type == i for i in load_type[4:-1]):
-                results_list = run_cycle(models, uploaded_data, type = "video")
+            elif any(file_type == i for i in load_type[3:8]):
+                type = "video"
+                results_list = run_cycle(models, uploaded_data, type, ensemle, route)
                 #Вывод после видоса, первый нолик - номер в списке, второй нолик - просто потому что--
                 #Вывод номеров классов - тупа словарь ------------------------------------------------
                 st.text(results_list[0][0].names)
@@ -100,12 +116,12 @@ def main():
     
     load()
     #load_camera()
-def run_cycle(models, image_original, type):
+def run_cycle(models, image_original, type, ensemle , route):
     if type == "image":
-        image_detected, results = detectors.run_full_cycle(models, image_original, type)
+        image_detected, results = detectors.run_full_cycle(models, image_original, type, ensemle , route)
         return image_detected, results
     elif type == "video":
-        results_list = detectors.run_full_cycle(models, image_original, type)
+        results_list = detectors.run_full_cycle(models, image_original, type, ensemle , route)
         return results_list
     
 def detect_objects(models, image, type = "segment"):
@@ -116,6 +132,11 @@ def load_img(img_file):
     img = img_file.getvalue()
     image = Image.open(BytesIO(img))
     return image
+
+if __name__ == '__main__':
+    models = load_models()
+    main()
+    
 
 if __name__ == '__main__':
     models = load_models()
